5efd2906105c4930258231a5ea83278938fa7795
Nicodemus Chan <nicodemus@phonester.com>
2023-11-24 22:22:01 +0800

    Add setup.py file

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rafflegame',
+    version='0.1',
+    packages=[''],
+    python_requires='>=3.10',
+    package_dir={'': 'src'},
+    url='https://www.linkedin.com/in/nicodemus-chan/',
+    author='Nicodemus Chan',
+    author_email='chan.nicodemus@gmail.com',
+    description='Raffle Lottery simulator'
+)
